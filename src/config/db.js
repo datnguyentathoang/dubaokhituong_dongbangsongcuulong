908568df7a109e6d7d446c1d891fd96e1f8d60e1
PinPin <nthdat1907@gmail.com>
2026-02-27 18:19:17 +0700

    update for deploy demo 6

--- src/config/db.js
+++ src/config/db.js
@@ -9,6 +9,7 @@ if (!process.env.DATABASE_URL) {
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  family: 4,
 });
 
 pool.on("error", (err) => {
